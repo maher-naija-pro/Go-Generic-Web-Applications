3fef5e531a390c0655e32a3c832e76fe5dcfc8a2
maher <mnstudios@hotmail.com>
2023-10-09 17:16:30 +0200

    update

--- main.go
+++ main.go
@@ -15,7 +15,8 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 // About is the handler for the about page
 func About(w http.ResponseWriter, r *http.Request) {
-	sum := addValues(2, 2)
+	sum := addValues(2, 2
+)
 	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
 }
 
