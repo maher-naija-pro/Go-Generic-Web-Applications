eaf0f1b80beaa23a6db3f6e0204a872d0d466f68
maher <mnstudios@hotmail.com>
2023-10-10 11:27:36 +0200

    update

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,16 +1,19 @@
 package handlers
 
 import (
-	"bookings-udemy/pkg/render"
 	"net/http"
+
+	"github.com/maher-naija-pro/Go-Generic-Web-Applications/pkg/render"
 )
 
 // Home is the handler for the home page
 func Home(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "home.page.tmpl")
+	return
 }
 
 // About is the handler for the about page
 func About(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "about.page.tmpl")
+	return
 }
