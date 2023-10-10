b9d0e7ab39669e5124e496ba204e2f26bc6a861c
maher <mnstudios@hotmail.com>
2023-10-10 11:44:57 +0200

    update

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -8,7 +8,7 @@ import (
 
 // RenderTemplate renders a template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl, "./templates/base.layout.tmpl")
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template:", err)
