82249d136cc7aaa20bbcab794d8a0aea22b6f698
maher <mnstudios@hotmail.com>
2023-10-14 18:30:56 +0200

    update

--- database/migration/000001_init_mg.up.sql
+++ database/migration/000001_init_mg.up.sql
@@ -1,9 +1,6 @@
-CREATE TABLE employees
-{
-id       BIGINT PRIMARY KEY,
-username VARCHAR(50) NOT NULL,
-passwordname  VARCHAR(50) NOT NULL
-
-};
-
-
+CREATE TABLE users
+(
+id             BIGINT PRIMARY KEY,
+username       VARCHAR(50) NOT NULL,
+passwordname   VARCHAR(50) NOT NULL
+);
