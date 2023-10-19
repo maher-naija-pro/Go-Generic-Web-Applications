0cbd04090983d9b58b72e9ef559d692bc7c3db7a
maher <mnstudios@hotmail.com>
2023-10-19 12:57:11 +0200

    update login logout

--- internal/handlers/login_handler.go
+++ internal/handlers/login_handler.go
@@ -1,71 +1,67 @@
 package handlers
 
 import (
-	"net/http"
-	"log"
 	"fmt"
-	"web_server/internal/models"
+	"log"
+	"net/http"
 	"web_server/internal/forms"
+	"web_server/internal/models"
 	"web_server/internal/render"
 )
 
 // login is the handler for the login page
 func (m *Repository) Login_Show(w http.ResponseWriter, r *http.Request) {
-	
-	render.RenderTemplate(w, r,"login.page.tmpl", &models.TemplateData{})
+
+	render.RenderTemplate(w, r, "login.page.tmpl", &models.TemplateData{})
 }
-
-
-
 
 // Login is the handler for the maher page
 func (m *Repository) Login(w http.ResponseWriter, r *http.Request) {
-    //Dump_req (r)
+	//Dump_req (r)
 	_ = m.App.Session.RenewToken(r.Context())
 	err := r.ParseForm()
-	if  err != nil {
-       log.Println(err )
+	if err != nil {
+		log.Println(err)
 	}
 
-	email :=  r.Form.Get("email")   
+	email := r.Form.Get("email")
 	password := r.Form.Get("password")
 
 	myForm := forms.New(r.PostForm)
-	
-	myForm.Required("email", "password")
-    myForm.IsEmail("email")
-	
-	if !myForm.Valid() { 
-		 log.Println("not valid")
-		 render.RenderTemplate(w,r,"login.page.tmpl",&models.TemplateData{
-          Forms: myForm,
-		})
-          return
-//		m.App.Session.Put(r.Context(), "error", "please give email")
-//		http.Redirect(w, r, "/error", http.StatusSeeOther)
-//		return
-//		}else if  !myForm.IsEmail("email") {
-//		m.App.Session.Put(r.Context(), "error", "not a valid email address")
-//		http.Redirect(w, r, "/error", http.StatusSeeOther)
-//		return
-//	
-//	} else if  !myForm.Has("password") {
-//		m.App.Session.Put(r.Context(), "error", "please give password")
-//		http.Redirect(w, r, "/error", http.StatusSeeOther)
-//		return
-	}
-	
 
-	ID, err := m.DB.AuthUser(email,password)
-	if err != nil {	
-        log.Println(err)
-		m.App.Session.Put(r.Context(), "error", fmt.Sprint(err)  )
+	myForm.Required("email", "password")
+	myForm.IsEmail("email")
+
+	if !myForm.Valid() {
+		log.Println("not valid")
+		render.RenderTemplate(w, r, "login.page.tmpl", &models.TemplateData{
+			Forms: myForm,
+		})
+		return
+		//		m.App.Session.Put(r.Context(), "error", "please give email")
+		//		http.Redirect(w, r, "/error", http.StatusSeeOther)
+		//		return
+		//		}else if  !myForm.IsEmail("email") {
+		//		m.App.Session.Put(r.Context(), "error", "not a valid email address")
+		//		http.Redirect(w, r, "/error", http.StatusSeeOther)
+		//		return
+		//
+		//	} else if  !myForm.Has("password") {
+		//		m.App.Session.Put(r.Context(), "error", "please give password")
+		//		http.Redirect(w, r, "/error", http.StatusSeeOther)
+		//		return
+	}
+
+	ID, err := m.DB.AuthUser(email, password)
+	if err != nil {
+		log.Println(err)
+		m.App.Session.Put(r.Context(), "error", fmt.Sprint(err))
 		http.Redirect(w, r, "/error", http.StatusSeeOther)
 		return
 	}
+	log.Println("your id", ID)
 
-
-	m.App.Session.Put(r.Context(),"user_id",ID) 
-	m.App.Session.Put(r.Context(),"flash","logged sucessful")
-    http.Redirect(w, r, "", http.StatusSeeOther)	
+	m.App.Session.Put(r.Context(), "user_id", ID)
+	m.App.Session.Put(r.Context(), "flash", "logged sucessful")
+	http.Redirect(w, r, "", http.StatusSeeOther)
 }
